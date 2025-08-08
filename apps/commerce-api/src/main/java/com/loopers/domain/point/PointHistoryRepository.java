f884c6b97dab825afe7eee7189b4b04aed931c29
h2jinee <wjsgmlwls97@gmail.com>
2025-08-08 13:27:57 +0900

    refactor: 사용하지 않는 Repository 제거

--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointHistoryRepository.java
+++
@@ -1,5 +0,0 @@
-package com.loopers.domain.point;
-
-public interface PointHistoryRepository {
-    // 도메인 특화 메서드들만 정의 (현재는 없음)
-}
--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointRepository.java
+++
@@ -1,5 +0,0 @@
-package com.loopers.domain.point;
-
-public interface PointRepository {
-    // 도메인 특화 메서드들만 정의 (현재는 없음)
-}
