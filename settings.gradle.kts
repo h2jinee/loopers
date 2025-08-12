f33bedcfa5eb624a79ebf284cd8293c60b215d90
h2jinee <wjsgmlwls97@gmail.com>
2025-08-13 00:42:20 +0900

    feat: Redis 캐시 인프라 구축 (Master-Replica)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "hoosinsa"
 include(
     ":apps:commerce-api",
     ":modules:jpa",
+    ":modules:redis",
     ":supports:jackson",
     ":supports:logging",
     ":supports:monitoring",
